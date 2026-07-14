74d0fc17d4d96b0928c936bcf3317014919adee6
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:36:33 +0800

    chore: remove scraper for deleted journal (ijpr)

--- scrapers/journals/ijprScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createTaylorFrancisScraper } from './utils/taylorfrancis.mjs';
-
-export const scraperObject = createTaylorFrancisScraper('International Journal of Production Research', 'ijpr', 'https://www.tandfonline.com/journals/tprs20');
