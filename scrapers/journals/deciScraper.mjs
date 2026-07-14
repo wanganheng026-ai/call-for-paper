96ed1c320d84e5442b97057b4805a53836ccfa2c
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:37:33 +0800

    chore: remove scraper for deleted journal (deci)

--- scrapers/journals/deciScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createWileyScraper } from './utils/wiley.mjs';
-
-export const scraperObject = createWileyScraper('Decision Sciences', 'deci', 'https://onlinelibrary.wiley.com/journal/deci/homepage/specialissues');
