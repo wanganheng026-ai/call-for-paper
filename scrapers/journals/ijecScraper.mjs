245f89d62035fdafbd14c2277ee4f6b964b4feea
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:36:28 +0800

    chore: remove scraper for deleted journal (ijec)

--- scrapers/journals/ijecScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createTaylorFrancisScraper } from './utils/taylorfrancis.mjs';
-
-export const scraperObject = createTaylorFrancisScraper('International Journal of Electronic Commerce', 'ijec', 'https://www.tandfonline.com/journals/ujec20');
