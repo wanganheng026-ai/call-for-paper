5dd0c42f42574e757e684bee4ec7bbf8a9a24c04
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:36:41 +0800

    chore: remove scraper for deleted journal (jors)

--- scrapers/journals/jorsScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createTaylorFrancisScraper } from './utils/taylorfrancis.mjs';
-
-export const scraperObject = createTaylorFrancisScraper('Journal of the Operational Research Society', 'jors', 'https://www.tandfonline.com/journals/tjor20');
