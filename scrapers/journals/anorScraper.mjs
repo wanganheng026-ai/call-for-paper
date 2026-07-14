955528e833f0d37ecd00909cff0d3896d061ada5
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:35:22 +0800

    chore: remove scraper for deleted journal (anor)

--- scrapers/journals/anorScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createSpringerScraper } from './utils/springer.mjs';
-
-export const scraperObject = createSpringerScraper('Annals of Operations Research', 'anor', 'https://link.springer.com/journal/10479');
