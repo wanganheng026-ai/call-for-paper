14135e6988f54bd3d4470329e43e73c26d4adf12
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:36:49 +0800

    chore: remove scraper for deleted journal (tems)

--- scrapers/journals/temsScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createIEEEScraper } from './utils/ieee.mjs';
-
-export const scraperObject = createIEEEScraper('IEEE Transactions on Engineering Management', 'tems', 'https://ieeexplore.ieee.org/xpl/RecentIssue.jsp?punumber=17');
