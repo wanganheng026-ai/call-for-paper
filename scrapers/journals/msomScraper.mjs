1a19fe1b5fd187b9f4aecf614de53b8da6beeff2
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:36:43 +0800

    chore: remove scraper for deleted journal (msom)

--- scrapers/journals/msomScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createINFORMSScraper } from './utils/informs.mjs';
-
-export const scraperObject = createINFORMSScraper('Manufacturing and Service Operations Management', 'msom', 'https://pubsonline.informs.org/page/calls-for-papers');
