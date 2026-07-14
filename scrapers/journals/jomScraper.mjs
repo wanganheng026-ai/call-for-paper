fcdbc814675339b6c9b6b743ed5ab95fdc1135b3
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:36:38 +0800

    chore: remove scraper for deleted journal (jom)

--- scrapers/journals/jomScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createWileyScraper } from './utils/wiley.mjs';
-
-export const scraperObject = createWileyScraper('Journal of Operations Management', 'jom', 'https://onlinelibrary.wiley.com/journal/jom/homepage/specialissues');
