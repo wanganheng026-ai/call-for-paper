bf53c17ddb9be56291c1eadaad1e30e19b5829ac
wanganheng026-ai <wanganheng026@gmail.com>
2026-07-14 23:36:31 +0800

    chore: remove scraper for deleted journal (ijpe)

--- scrapers/journals/ijpeScraper.mjs
+++
@@ -1,3 +0,0 @@
-import { createElsevierScraper } from './utils/elsevier.mjs';
-
-export const scraperObject = createElsevierScraper('International Journal of Production Economics', 'ijpe', 'international-journal-of-production-economics');
